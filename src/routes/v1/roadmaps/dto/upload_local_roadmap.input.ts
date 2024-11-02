9e3070181d4c2c50ae0297d6768fd7e06f387753
andukdahacker <82881040+andukdahacker@users.noreply.github.com>
2024-11-02 16:43:03 +0700

    update

--- src/routes/v1/roadmaps/dto/upload_local_roadmap.input.ts
+++ src/routes/v1/roadmaps/dto/upload_local_roadmap.input.ts
@@ -2,7 +2,7 @@ import { Static, Type } from "@sinclair/typebox";
 import { RoadmapSchema } from "./roadmap.schema";
 
 export const UploadLocalRoadmapInputSchema = Type.Object({
-  roadmaps: Type.Array(RoadmapSchema),
+  roadmaps: Type.Array(Type.Omit(RoadmapSchema, ['user', 'userId'])),
 });
 
 export type UploadLocalRoadmapInput = Static<
--- src/types/nullable.ts
+++ src/types/nullable.ts
@@ -2,3 +2,6 @@ import { TSchema, Type } from "@sinclair/typebox";
 
 export const Nullable = <T extends TSchema>(schema: T) =>
   Type.Union([schema, Type.Null()]);
+
+export const NullableAndOptional = <T extends TSchema>(schema: T) =>
+  Type.Union([schema, Type.Null(), Type.Optional(schema)]);
